34608f0fbe654c35537dda78a5f4bd71d30473e3
ms0223900 <ms0223900@gmail.com>
2023-10-18 22:19:25 +0800

    add fn-mario sample

---
+++ fn-mario.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_size(void); 
+void print_grid(int block_amount); 
+
+int main(void) {
+  int size = get_size();
+  print_grid(size);
+}
+
+int get_size(void) {
+  int QUESTION_BLOCK_AMOUNT;
+
+  do {
+    QUESTION_BLOCK_AMOUNT = get_int("How many blocks?\n");
+  } while(QUESTION_BLOCK_AMOUNT < 1);
+
+  return QUESTION_BLOCK_AMOUNT;
+}
+
+void print_grid(int block_amount) {
+  for(int i = 0; i < block_amount; i++) {
+    for(int j = 0; j < block_amount; j++) {
+      printf("#");  
+    }
+    printf("\n");
+  }
+}
