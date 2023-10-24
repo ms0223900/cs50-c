e20404c27420271717893e45ef6e38ab13c30670
ms0223900 <ms0223900@gmail.com>
2023-10-24 20:50:14 +0800

    update scores to use loop average

--- scores.c
+++ scores.c
@@ -1,20 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) {
-  // int score1 = 72;
-  // int score2 = 73;
-  // int score3 = 33;
-  // float avg = (score1 + score2 + score3) / (float) 3;
+const int SCORE_COUNT = 5;
+float average(int arr[]); // 事先宣告
 
-  int scores[3]; // 宣告array
-  int SCORE_COUNT = 3;
+int main(void) {
+  int scores[SCORE_COUNT]; // 宣告array, 在C中，一定要事先規範好array的長度
 
   for (int i = 0; i < SCORE_COUNT; i++)
   {
     scores[i] = get_int("Score: ");   
   }
-  float avg = (scores[0] + scores[1] + scores[2]) / (float) SCORE_COUNT;
+  // float avg = (scores[0] + scores[1] + scores[2]) / (float) SCORE_COUNT;
+  float avg = average(scores);
 
   printf("Avg score: %f", avg);
+}
+
+float average(int arr[]) {
+  int sum = 0;
+  for (int i = 0; i < SCORE_COUNT; i++)
+  {
+    sum += arr[i];
+  }
+  return sum / (float) SCORE_COUNT;
 }
