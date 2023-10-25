4c1de138eb57f22fbc86de09a967aa5ab54c1db9
ms0223900 <ms0223900@gmail.com>
2023-10-25 22:46:47 +0800

    add uppercase sample

---
+++ uppercase.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+char END_CHAR = '\0';
+
+char* concat_str(char* s1, char* s2) {
+  int i;
+  int s1_len = strlen(s1);
+
+  for (i = 0; s2[i] != '\0'; i++)
+  {
+    s1[i + s1_len] = s2[i];
+  }
+  s1[i + s1_len] = '\0';
+
+  return s1;
+}
+
+char* char_to_str(char _char) {
+  static char str[2];
+  str[0] = _char;
+  str[1] = '\0'; 
+
+  return str;
+}
+
+int main(void) {
+  string name = get_string("What's name ? \n");
+
+  char uppercaseName[1000];
+  strcpy(uppercaseName, "");
+
+  int i = 0;
+  while (i < strlen(name))
+  {
+    char c = name[i];
+    if(c >= 'a' && c <= 'z') {
+      char uppercase_char = c - 32;
+      string uppercase_str = char_to_str(uppercase_char);
+
+      concat_str(uppercaseName, uppercase_str);
+    } else {
+      concat_str(uppercaseName, &c);
+    }
+    i++;
+  }
+  printf("Uppercase name: %s\n", uppercaseName);
+}
