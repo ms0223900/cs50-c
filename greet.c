344bcc3fd5489daa7d0fe36ff16d7ffd0feb1aa2
ms0223900 <ms0223900@gmail.com>
2023-10-26 20:51:37 +0800

    add main args sample

---
+++ greet.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int arg_count, char* argv[]) {
+  printf("Called argments: %i\n", arg_count);
+  printf("Called file: %s\n", argv[0]);
+  printf("%s--%s\n", argv[1], argv[2]);
+}
